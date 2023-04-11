84e6adc276c95f03583b14fc7d92ded231e5384c
hongdat1601 <hongdat1601@gmail.com>
2023-04-11 21:58:17 +0700

    update delete user

--- WebDeCuong.Api/Controllers/UserController.cs
+++ WebDeCuong.Api/Controllers/UserController.cs
@@ -35,6 +35,18 @@ namespace WebDeCuong.Api.Controllers
             }
         }
 
+        [HttpPut]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> UpdateUser([FromBody] UpdateUserModel model)
+        {
+            var result = await _userRepository.UpdateUser(model);
+            if (result.Status.CompareTo(Status.Error) == 0)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+
         [HttpPut("ResetPassword")]
         [Authorize(Roles = "Admin")]
         public async Task<IActionResult> ResetPassword([FromBody] string email)
@@ -45,7 +57,7 @@ namespace WebDeCuong.Api.Controllers
             return Ok(result);
         }
 
-        [HttpPut("changePassword")]
+        [HttpPut("ChangePassword")]
         public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordModel model)
         {
             var result = await _userRepository.ChangePassword(model);
@@ -85,20 +97,9 @@ namespace WebDeCuong.Api.Controllers
             return Ok(result);
         }
 
-        [HttpPut]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> UpdateUser([FromBody] UpdateUserModel model)
-        {
-            var result = await _userRepository.UpdateUser(model);
-            if(result.Status.CompareTo(Status.Error) == 0)
-            {
-                return BadRequest(result);
-            }
-            return Ok(result);
-        }
 
-        [HttpDelete("{email}")]
-        public async Task<IActionResult> DeleteUser(string email)
+        [HttpDelete()]
+        public async Task<IActionResult> DeleteUser([FromBody] string email)
         {
             var result = await _userRepository.DeleteUser(email);
             if(result.Status.CompareTo(Status.Error) == 0)
